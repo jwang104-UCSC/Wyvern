4aaf81584f4cb9a89bfdbd975fba26fef666533e
Wesley Kok <ryutarro123@gmail.com>
2018-05-25 07:07:19 -0700

    Put the functions inside the var instead in Shop.js

--- Wyvern/app/src/main/assets/www/src/Shop.js
+++ Wyvern/app/src/main/assets/www/src/Shop.js
@@ -1,23 +1,26 @@
 var Shop = 
 {
-	create: function(){
+	create: function()
+	{
 		game.add.sprite(0, 0, 'screen-bg');
 		startButton   = createButton("Start Game", 15, game.world.width*0.5, game.world.height*0.5, 
 						80, 30, function(){game.state.start('Howto')});
 		upgradeLives  = createButton("Lives", 15, game.world.width*0.5, game.world.height*0.7, 
-						80, 30, function(){livesPlus()});
+						80, 30, function(){Shop.livesPlus()});
 		upgradeSpread = createButton("Spread", 15, game.world.width*0.5, game.world.height*0.9, 
-						80, 30, function(){spreadPlus()});
+						80, 30, function(){Shop.spreadPlus()});
+	},
+
+	//Copy paste upgrade functions
+	livesPlus: function()
+	{
+		lives++;
+		game.state.start('Game');
+	},
+
+	spreadPlus: function()
+	{
+		shotSpread++;
+		game.state.start('Game');
 	}
-}
-
-//Copy paste upgrade functions
-function livesPlus() {
-	   lives++;
-	   game.state.start('Game');
-}
-
-function spreadPlus() {
-	   shotSpread++;
-	   game.state.start('Game');
 }
